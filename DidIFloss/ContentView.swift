7033d5f3c65b65eca00bcd374596171565d98b0b
Matheus Migge <matheusmigge@gmail.com>
2023-12-27 15:53:14 -0300

    Initial Commit

---
+++ DidIFloss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DidIFloss
+//
+//  Created by Matheus Migge on 27/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DidIFloss/DidIFlossApp.swift
@@ -0,0 +1,17 @@
+//
+//  DidIFlossApp.swift
+//  DidIFloss
+//
+//  Created by Matheus Migge on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DidIFlossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
