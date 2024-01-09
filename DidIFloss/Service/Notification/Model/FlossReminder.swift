24d22d789ff7b54e2257ac400f944feabefdd180
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-01-09 12:07:27 -0300

    feature(Notifications): refactoring NotificationModel

--- DidIFloss/Service/Notification/Model/FlossReminder.swift
+++ DidIFloss/Service/Notification/Model/FlossReminder.swift
@@ -9,42 +9,36 @@ import Foundation
 import UserNotifications
 
 extension Notification {
-    struct FlossReminder: NotificationModel {
-        let id: String
-        let titleMessage: String
-        var subTitle: String?
-        let bodyMessage: String
-        let timeInterval: TimeInterval
-        let shouldRepeat: Bool
-        let sound: UNNotificationSound = .default
+    
+    struct FlossReminder {
         
-        static func getAllNotifications() -> [FlossReminder] {
-            return [.twoDays, .oneWeek, .twoWeek]
+        static func getAllNotifications() -> [NotificationModel] {
+            return [self.twoDays, self.oneWeek, self.twoWeek]
         }
-
-        static let twoDays = FlossReminder(
-            id: "twoDaysInactivityNotification",
-            titleMessage: "Don't lose track! 🦷",
-            bodyMessage: "You are doing great! Don't forget to floss today and register it",
-            timeInterval: 172800,
-            shouldRepeat: false
+        
+        
+        static let twoDays = NotificationModel(id: "twoDaysInactivityNotification",
+                                               titleMessage: "Don't lose track! 🦷",
+                                               bodyMessage:  "You are doing great! Don't forget to floss today and register it",
+                                               timeInterval: 172800,
+                                               shouldRepeat: false
         )
-
-        static let oneWeek = FlossReminder(
-            id: "oneWeekInactivityNotification",
-            titleMessage: "It's been a week... 🗓️",
-            bodyMessage: "We are missing you! How about we floss today?",
-            timeInterval: 604800,
-            shouldRepeat: false
+        
+        static let oneWeek = NotificationModel(id: "oneWeekInactivityNotification",
+                                               titleMessage: "It's been a week... 🗓️",
+                                               bodyMessage:  "We are missing you! How about we floss today?",
+                                               timeInterval: 604800,
+                                               shouldRepeat: false
         )
-
-        static let twoWeek = FlossReminder(
-            id: "twoWeekInactivityNotification",
-            titleMessage: "Hey Stranger... 🫂",
-            bodyMessage: "We can help you with your oral hygiene. Let's floss those teeth!",
-            timeInterval: 1209600,
-            shouldRepeat: true
+        
+        static let twoWeek = NotificationModel(id: "twoWeekInactivityNotification",
+                                               titleMessage: "Hey Stranger... 🫂",
+                                               bodyMessage: "We can help you with your oral hygiene. Let's floss those teeth!",
+                                               timeInterval: 1209600,
+                                               shouldRepeat: true
         )
+        
+        
     }
-
+    
 }
--- DidIFloss/Service/Notification/Model/NotificationModel.swift
+++ DidIFloss/Service/Notification/Model/NotificationModel.swift
@@ -8,28 +8,27 @@
 import Foundation
 import UserNotifications
 
-protocol NotificationModel {
-    var id: String { get }
-    var titleMessage: String { get }
-    var bodyMessage: String { get }
-    var subTitle: String? { get }
-    var timeInterval: TimeInterval { get }
-    var shouldRepeat: Bool { get }
-    var sound: UNNotificationSound { get }
+
+struct NotificationModel {
+    let id: String
+    var titleMessage: String
+    var bodyMessage: String
+    var subTitle: String?
+    var timeInterval: TimeInterval
+    var shouldRepeat: Bool
+    var sound: UNNotificationSound = .default
     
-}
-
-extension NotificationModel {
+    
     var content: UNNotificationContent {
-        let content = UNMutableNotificationContent()
-        content.title = titleMessage
-        content.body = bodyMessage
-        content.sound = sound
-
-        if let safeSubTitle = subTitle {
-            content.subtitle = safeSubTitle
-        }
-        
-        return content
-    }
+          let content = UNMutableNotificationContent()
+          content.title = titleMessage
+          content.body = bodyMessage
+          content.sound = sound
+  
+          if let safeSubTitle = subTitle {
+              content.subtitle = safeSubTitle
+          }
+  
+          return content
+      }
 }
