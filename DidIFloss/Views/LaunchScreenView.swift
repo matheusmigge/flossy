fef60b01ee1219fb182bc73a47566103558aae1c
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-01-24 16:07:46 -0300

    feature(LaunchScreen): improving animation

--- DidIFloss/Views/LaunchScreenView.swift
+++ DidIFloss/Views/LaunchScreenView.swift
@@ -9,69 +9,49 @@ import SwiftUI
 
 struct LaunchScreenView: View {
     
-    @State var isAnimating: Bool = false
+    @State private var isAnimating: Bool = false
     var animationOver: () -> Void
     
     var body: some View {
         ZStack {
-            
             BackgroundView()
             
             homeScreenTextContent
-                .scaleEffect(isAnimating ? 3 : 1)
-            
+                .scaleEffect(isAnimating ? 40 : 1)
             
         }
         .ignoresSafeArea()
         .onAppear {
-            withAnimation(.easeInOut(duration: 2)) {
-                isAnimating = true
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    animationOver()
-                }
-                                              }
-           
+            startAnimation()
         }
     }
     
-    var homeScreenTextContent: some View {
-        
-        VStack(spacing: 220) {
-            //            Spacer()
-            
-            VStack(spacing: -50) {
-                Text("did I")
-                Text("floss?")
-            }
-            .font(.custom(Constants.FontNames.borel, size: 60))
-            .padding(30)
-            .padding(.bottom, -20)
-            .background(Color.flossLightYellow)
-            .foregroundStyle(Color.black)
-            .multilineTextAlignment(.center)
-            .rotationEffect(.degrees(7))
-            .shadow(color: Color.black.opacity(0.5), radius: 5)
-            
-            //            Button {
-            //                withAnimation(.linear(duration: 0.5)) {
-            //                    self.currentContent = .content
-            //                }
-            //            } label: {
-            //                Text("Enter")
-            //                    .font(.system(size: 25))
-            //                    .fontWeight(.black)
-            //                    .foregroundStyle(Color.black)
-            //                    .padding(.horizontal,80)
-            //                    .padding(.vertical,20)
-            //                    .background(Color.flossLightYellow)
-            //                    .cornerRadius(20)
-            //                    .shadow(color: Color.black.opacity(0.5), radius: 5)
-            //            }
-            //            .buttonStyle(.plain)
-            //
-            //            Spacer()
+    private var homeScreenTextContent: some View {
+        VStack(spacing: -50) {
+            Text("did I")
+            Text("floss?")
         }
+        .font(.custom(Constants.FontNames.borel, size: 60))
+        .padding(30)
+        .padding(.bottom, -20)
+        .background(Color.flossLightYellow)
+        .foregroundStyle(Color.black)
+        .multilineTextAlignment(.center)
+        .rotationEffect(.degrees(7))
+        .shadow(color: Color.black.opacity(0.5), radius: 5)
+        
     }
+    
+    private func startAnimation() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation(.easeInOut(duration: 1)) {
+                    isAnimating = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        animationOver()
+                    }
+                }
+            }
+        }
 }
 
 #Preview {
