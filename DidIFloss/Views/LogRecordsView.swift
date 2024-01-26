336544fcec1b91042da2f5edf5c5146b8d3dbec4
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-01-26 17:04:25 -0300

    feature(LogRecordsView): refactoring code

--- DidIFloss/Views/LogRecordsView.swift
+++ DidIFloss/Views/LogRecordsView.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 extension ContentViewModel: CalendarViewDelegate {
     func didSelectDate(_ date: Date) {
-        selectedDate = selectedDate == date ? nil : date
+        withAnimation {
+            selectedDate = selectedDate == date ? nil : date
+        }
     }
     
     func removeRecordAt(indexSet: IndexSet) {
         records.remove(atOffsets: indexSet)
-//        guard let index = indexSet.first else { return }
-//        persistance.deleteFlossRecord(records[index])
+        //        guard let index = indexSet.first else { return }
+        //        persistance.deleteFlossRecord(records[index])
     }
     
     func removeRecord(_ record: FlossRecord) {
         records.removeAll { $0 == record }
-//        persistance.deleteFlossRecord(record)
+        //        persistance.deleteFlossRecord(record)
     }
     
     var sectionRecords: [FlossRecord] {
@@ -36,6 +38,25 @@ struct LogRecordsView: View {
     
     @ObservedObject var viewModel: ContentViewModel
     
+    var body: some View {
+        List {
+            Section("Calendar") {
+                CalendarView(records: $viewModel.records,
+                             style: .month,
+                             delegate: viewModel)
+            }
+            
+            Section(sectionLabel) {
+                if viewModel.sectionRecords.isEmpty {
+                    noRecordsView
+                } else {
+                    recordsListView
+                }
+            }
+        }
+        .buttonStyle(.borderless)
+    }
+    
     var sectionLabel: String {
         if let date = viewModel.selectedDate {
             return "Records for \(date.dayAndMonthFormatted)"
@@ -44,35 +65,43 @@ struct LogRecordsView: View {
         }
     }
     
-    
-    var body: some View {
-        List {
-            Section("Calendar") {
-                CalendarView(records: $viewModel.records, style: .month, delegate: viewModel)
+    private var noRecordsView: some View {
+        HStack {
+            Spacer()
+            
+            VStack(alignment: .center, spacing: 10) {
+                
+                Image(systemName: "tray.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.secondary)
+                    .frame(width: 30)
+                
+                Text("No records")
+                    .foregroundStyle(.secondary)
+                    .font(.headline)
             }
             
-            Section(sectionLabel) {
-                if viewModel.sectionRecords.isEmpty {
-                    Text("No records")
-                } else {
-                    ForEach(viewModel.sectionRecords) { record in
-                        Text(record.date.dayAndMonthFormatted)
-                            .contextMenu(menuItems: {
-                                Button(role: .destructive) {
-                                    viewModel.removeRecord(record)
-                                } label: {
-                                    Text("Delete")
-                                }
-                            })
-                        
-                    }
-                    .onDelete(perform: { indexSet in
-                        viewModel.removeRecordAt(indexSet: indexSet)
-                    })
-                }
-            }
+            Spacer()
         }
-        .buttonStyle(.borderless)
+        .frame(height: 100)
+    }
+    
+    private var recordsListView: some View {
+        ForEach(viewModel.sectionRecords) { record in
+            Text(record.date.dayAndMonthFormatted)
+                .contextMenu(menuItems: {
+                    Button(role: .destructive) {
+                        viewModel.removeRecord(record)
+                    } label: {
+                        Text("Delete")
+                    }
+                })
+            
+        }
+        .onDelete(perform: { indexSet in
+            viewModel.removeRecordAt(indexSet: indexSet)
+        })
     }
 }
 
