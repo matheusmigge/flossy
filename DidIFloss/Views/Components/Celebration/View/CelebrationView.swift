d29e66642fba665f17e732fce3253f7456bed01f
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-02-01 19:06:05 -0300

    feature(Celebration): turning celebrationView responsive to screenSize

--- DidIFloss/Views/Components/Celebration/View/CelebrationView.swift
+++ DidIFloss/Views/Components/Celebration/View/CelebrationView.swift
@@ -10,24 +10,30 @@ import SwiftUI
 
 struct CelebrationView: View {
     
-    let confettiCount = 30
+    private let confettiCount: Int
     @State var isAnimating: Bool = false
     
     weak var delegate: CelebrationViewDelegate?
     
+    init(confettiCount: Int = 30, delegate: CelebrationViewDelegate? = nil) {
+        self.confettiCount = confettiCount
+        self.delegate = delegate
+    }
+    
     var body: some View {
-        ZStack {
-            ForEach(0..<confettiCount, id: \.self) { index in
-                ConfettiPieceView()
-                    .offset(x: isAnimating ? CGFloat.random(in: -250...250) : -0,
-                            y: isAnimating ?  CGFloat.random(in: -350...350) : 400)
-                    .rotationEffect(isAnimating ? .degrees(Double.random(in: -45...45)) : .degrees(0))
-                
+        GeometryReader { geo in
+            ZStack {
+                ForEach(0..<confettiCount, id: \.self) { index in
+                    ConfettiPieceView()
+                        .offset(x: isAnimating ? CGFloat.random(in: geo.size.width * 0.1...geo.size.width * 0.9) : geo.size.width / 2,
+                                y: isAnimating ?  CGFloat.random(in: geo.size.height*0.1...geo.size.height*0.75) : geo.size.height + 10)
+                        .rotationEffect(isAnimating ? .degrees(Double.random(in: -15...15)) : .degrees(0))
+                }
             }
         }
         .ignoresSafeArea()
         .onAppear {
-            withAnimation(.easeInOut(duration: 2)) {
+            withAnimation(.spring(duration: 2)) {
                 isAnimating = true
             }
             
