7539441196572b75b260e0e9445b762c98dd9ac4
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-02-07 12:18:12 -0300

    creating Color extension file

---
+++ DidIFloss/Extensions/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  DidIFloss
+//
+//  Created by Lucas Migge on 07/02/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let flossSkyBlue = Color(Constants.ColorNames.skyBlue)
+    static let flossLightYellow = Color(Constants.ColorNames.lightYellow)
+    static let flossFlamingoPink = Color(Constants.ColorNames.flamingoPink)
+    static let flossGreenyBlue = Color(Constants.ColorNames.greenyBlue)
+}
--- DidIFloss/Model/Constants.swift
+++ DidIFloss/Model/Constants.swift
@@ -18,19 +18,12 @@ struct Constants {
         static let toothPink = "pink-tooth"
         static let toothYellow = "yellow-tooth"
     }
-}
-
-extension Color {
     
-    private struct ColorNames {
+    struct ColorNames {
         static let skyBlue = "sky-blue"
         static let lightYellow = "light-yellow"
         static let flamingoPink = "flamingo-pink"
         static let greenyBlue = "greeny-blue"
     }
-    
-    static let flossSkyBlue = Color(ColorNames.skyBlue)
-    static let flossLightYellow = Color(ColorNames.lightYellow)
-    static let flossFlamingoPink = Color(ColorNames.flamingoPink)
-    static let flossGreenyBlue = Color(ColorNames.greenyBlue)
 }
+
