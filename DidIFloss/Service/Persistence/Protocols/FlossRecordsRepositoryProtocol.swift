bca70a16f2d3c107cde423df5fcc6f0e22862e50
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-03-26 21:08:54 -0300

    protocols refactoring

---
+++ DidIFloss/Service/Persistence/Protocols/FlossRecordsRepositoryProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  FlossRecordsRepository.swift
+//  DidIFloss
+//
+//  Created by Lucas Migge on 26/03/24.
+//
+
+import Foundation
+
+protocol FlossRecordsRepositoryProtocol: AnyObject {
+    
+    /// Fetches the floss records and provides them to the specified handler.
+    ///
+    /// - Parameter handler: A closure that receives an array of FlossRecord.
+    func getFlossRecords(handler: @escaping ([FlossRecord]) -> Void)
+    
+}
